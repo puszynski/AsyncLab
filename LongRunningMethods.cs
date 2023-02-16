60fd359c8125d62754ad873c97fa5f57f2b4b16a
Jakub Puszyński <jakub.puszynski@efitness.com>
2023-02-16 21:19:41 +0100

    Add project files.

---
+++ LongRunningMethods.cs
@@ -0,0 +1,26 @@
+﻿namespace AsyncLab
+{
+    internal static class LongRunningMethods
+    {
+
+        internal static async Task<bool> LongRunTaskOfDuration3s()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            Console.WriteLine("LongRunTask3s ended");
+            return true;
+        }
+        internal static async Task<bool> LongRunTaskOfDuration4s()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(4));
+            Console.WriteLine("LongRunTask4s ended");
+            return true;
+        }
+
+        internal static async Task<bool> LongRunTaskOfDuration6s()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(6));
+            Console.WriteLine("LongRunTask6s ended");
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using AsyncLab;
+
+
+Console.WriteLine("Press 1 and enter => Run all long running tasks with await operators.");
+Console.WriteLine("Press 2 and enter => Run all long running tasks with no await operators.");
+Console.WriteLine("Press 3 and enter => Run all long running tasks with no await operators, but await for all tasks in next steps.");
+
+int menuOption = Convert.ToInt32(Console.ReadLine());
+
+switch (menuOption)
+{
+	case 1:
+		Console.WriteLine("Start option 1 (note: Tasks will run synchronously one after another).");
+        await LongRunningMethods.LongRunTaskOfDuration3s();
+        await LongRunningMethods.LongRunTaskOfDuration4s();
+        await LongRunningMethods.LongRunTaskOfDuration6s();
+		Console.WriteLine("End option 1. Press key to exit program.");
+        Console.ReadKey();
+        break;
+
+	case 2:
+		Console.WriteLine("Start option 2 (beware! If console app will be closed (press any key), background threads will break before done its jobs).");
+        LongRunningMethods.LongRunTaskOfDuration3s();
+        LongRunningMethods.LongRunTaskOfDuration4s();
+        LongRunningMethods.LongRunTaskOfDuration6s();
+        Console.WriteLine("End option 2. Press key to exit program.");
+        Console.ReadKey();
+        break;
+
+	case 3:
+        Console.WriteLine("Start option 3 (note: Task will run asynchroniusly, and await operators will wait untill all tasks will be done).");
+        var task3s = LongRunningMethods.LongRunTaskOfDuration3s();
+        var task4s = LongRunningMethods.LongRunTaskOfDuration4s();
+        var task6s = LongRunningMethods.LongRunTaskOfDuration6s();
+
+        await task3s;
+        await task4s;
+        await task6s;
+        Console.WriteLine("End option 2. Press key to exit program.");
+        Console.ReadKey();
+        break;
+
+	default:
+        Console.WriteLine("Wrong user input. Try again.");
+		break;
+}
+
+Console.WriteLine("Program closed.");
